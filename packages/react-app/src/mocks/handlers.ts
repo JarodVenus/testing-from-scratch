06997dc1e230ad3199ebcb6bc0b09c1ec9094747
Jarod <jarod78972@gmail.com>
2025-05-13 16:21:44 +0200

    feat: replaces rest from msw by http

--- packages/react-app/src/mocks/handlers.ts
+++ packages/react-app/src/mocks/handlers.ts
@@ -1,20 +1,24 @@
-import { rest } from 'msw';
- import { User } from '../hooks/useUsers';
- let users: User[] = [
- { id: 1, name: 'Alice' },
- { id: 2, name: 'Bob' },
- ];
- export const handlers = [
- // GET /users
-rest.get('/users', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(users));
- }),
- // POST /users
- rest.post('/users', async (req, res, ctx) => {
- const { name } = await req.json<{ name: string }>();
- if (!name) res(ctx.status(400), ctx.json({ error: 'name is required' }));
- const newUser = { id: users.length + 1, name };
- users.push(newUser);
- return res(ctx.status(201), ctx.json(newUser));
- }),
- ];
+import { http, HttpResponse } from "msw"
+import { User } from "../hooks/useUsers"
+let users: User[] = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+]
+export const handlers = [
+	// GET /users
+	http.get("/users", () => {
+		return HttpResponse.json(users, { status: 200 })
+	}),
+	// POST /users
+	http.post("/users", async ({ request }) => {
+		const { name } = (await request.json()) as { name: string }
+
+		if (!name)
+			return HttpResponse.json({ message: "Name is required" }, { status: 400 })
+
+		const newUser = { id: users.length + 1, name }
+		users.push(newUser)
+
+		return HttpResponse.json(newUser, { status: 201 })
+	}),
+]
